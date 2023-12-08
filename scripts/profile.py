3b3642159c416d870606950515c3f69fa14912dd
Your Name <yic7@illinois.edu>
2023-12-07 23:52:19 -0600

    committing partial scripts

--- scripts/profile.py
+++ scripts/profile.py
@@ -2,7 +2,7 @@ import pandas as pd
 from ydata_profiling import ProfileReport
 
 df = pd.read_csv("data/wdbc.data")
-profile = ProfileReport(df, title="Profiling Report")
+profile = ProfileReport(df, title="Wisconsin Breast Cancer from UCI Profiling Report")
 profile.to_file("report.html")
-print(df.head())
+
 
